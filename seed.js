ba8560719dfc7923de62bf9e770e9080ba330d8e
Chloe Chong <chloechong.us@gmail.com>
2018-05-01 10:10:57 -0400

    Updated thens

--- seed.js
+++ seed.js
@@ -29,6 +29,7 @@ const arugula = Vegetable.create({
     plantedOn: new Date('2018-01-03')
 })
 
+
 const chloe = broccoli.then(veg => {
     return Gardener.create({
         name: 'Chloe',
@@ -45,6 +46,8 @@ const jenny = arugula.then(veg => {
     })
 })
 
+const allVeg = Promise.all([tomato, broccoli, arugula]);
+
 const plot1 = jenny.then(gardener => {
     return Plot.create({
         size: 100,
@@ -52,7 +55,9 @@ const plot1 = jenny.then(gardener => {
         gardenerId: gardener.id
     })
 })
-// .then(plot => plot.addVegetables([broccoli]));
+
+// Talked to Gabe and he said we could also do what you suggested - created variables then assigning them inside the 'then'
+let createdPlot, veggie, etc
 
 const plot2 = chloe.then(gardener => {
     return Plot.create({
@@ -60,11 +65,15 @@ const plot2 = chloe.then(gardener => {
         shaded: true,
         gardenerId: gardener.id
     })
+}).then(plot => {
+    createdPlot = plot
+    return allVeg.then(result => {
+        return plot.addVegetables(result);
+    })
 })
 
-const vegetables = Vegetable.findAll();
-console.log(vegetables);
-// const PlotVeg = db.models('plotVeg');
+
+// const vegetables = Vegetable.findAll();
 
 
 // BULLSHIT
